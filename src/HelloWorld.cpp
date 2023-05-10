525841d3b22ed88ec4ef72e36a9851b601038e8e
Kochetov.mlk <mikhail.kochetov.04@list.ru>
2023-05-10 20:31:42 +0300

    изменение code style в файле hello_world.cpp

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    // Запросить имя пользователя
-    std::string name;
-    std::cout << "Введите имя пользователя: ";
-    std::cin >> name;
-    
-    // Вывести приветствие
-    std::cout << "Hello world from @" << name << std::endl;
+int
+main()
+{
+  // Запросить имя пользователя
+  std::string name;
+  std::cout << "Введите имя пользователя: ";
+  std::cin >> name;
 
-    return 0;
+  // Вывести приветствие
+  std::cout << "Hello world from @" << name << std::endl;
+
+  return 0;
 }
